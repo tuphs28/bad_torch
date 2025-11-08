01b231aaa0bd6564c85d11cd4c43c3a70ab458cb
tuphs28 <28tbush@gmail.com>
2025-11-08 15:22:23 +0000

    Added scalar multiplication, added basic api improvement with arithmetic ops

--- badtorch/autograd/core.py
+++ badtorch/autograd/core.py
@@ -17,9 +17,12 @@ class Tensor:
 
     def __init__(self, data, requires_grad = False):
 
-        # Temporarily: force all input data to be float32 numpy arrays
-        assert isinstance(data, np.ndarray), f"Expected numpy array, got {type(data)}"
-        assert data.dtype == np.float32, f"Expected float32 array, got {data.dtype}"
+        if isinstance(data, (int, float)):
+            data = np.array(data, dtype=np.float32)
+        elif isinstance(data, np.ndarray):
+            assert data.dtype == np.float32, f"Expected float32 for numpy datatype, got {data.dtype}"
+        else:
+            raise ValueError(f"Tensor class does not currently support data of type {type(data)}")
 
         self.data = data
         self.grad = np.zeros_like(data)
@@ -37,10 +40,10 @@ class Tensor:
 
         self.grad = np.ones_like(self.data)
 
-        visited_ancestors = []
+        visited_ancestors = set()
         ordered_ancestors = []
         def topo_sort(current_tensor):
-            visited_ancestors.append(current_tensor)
+            visited_ancestors.add(current_tensor)
             for parent in current_tensor._parents:
                 if parent not in visited_ancestors:
                     topo_sort(parent)
@@ -51,7 +54,58 @@ class Tensor:
             for backward_fnc in tensor._backward_fncs:
                 backward_fnc()
 
-    def matrix_vector_multiply(self, other):
+
+    def __add__(self, other):
+
+        assert isinstance(other, Tensor), f"Expected a Tensor, got a {type(other)} which is not currently supported"
+
+        if (len(self.shape) == 1) and (len(other.shape) == 1) and (self.shape[0] == other.shape[0]):
+            result = self._vector_vector_add(other)
+        else:
+            raise NotImplementedError("Tensor addition for a {self.shape} Tensor with a {other.shape} Tensor not currently implemented")
+
+        return result
+    
+
+    def __sub__(self, other):
+
+        assert isinstance(other, Tensor), f"Expected a Tensor, got a {type(other)} which is not currently supported"
+
+        return self + (Tensor(-1, requires_grad=other.requires_grad) * other)
+    
+
+    def __mul__(self, other):
+
+        assert isinstance(other, Tensor), f"Expected a Tensor, got a {type(other)} which is not currently supported"
+
+        if (len(self.shape) == 0) and (len(other.shape) == 1): # scalar-vector multiply
+            result = self._scalar_vector_multiply(other)
+        elif (len(self.shape) == 1) and (len(other.shape) == 0): # vector-scalar multiply
+            result = other._scalar_vector_multiply(self)
+        elif (len(self.shape) == 0) and (len(other.shape) == 2): # scalar-matrix multiply
+            result = self._scalar_matrix_multiply(other)
+        elif (len(self.shape) == 2) and (len(other.shape) == 0): # scalar-matrix multiply
+            result = other._scalar_matrix_multiply(self)
+        else:
+            raise NotImplementedError(f"Multiplication for a {self.shape} Tensor with a {other.shape} Tensor not currently implemented")
+        
+        return result
+    
+    def __matmul__(self, other):
+
+        assert isinstance(other, Tensor), f"Expected a Tensor, got a {type(other)} which is not currently supported"
+
+        if (len(self.shape) == 2) and (len(other.shape) == 1) and (self.shape[-1] == other.shape[0]): # Matrix-vector multiply
+            result = self._matrix_vector_matrixmultiply(other)
+        elif (len(self.shape) == 1) and (len(other.shape) == 1) and (self.shape[0] == other.shape[0]): # Vector-vector multiply 
+            result = self._vector_vector_matrixmultiply(other)
+        else:
+            raise NotImplementedError("Tensor multiply for a {self.shape} Tensor with a {other.shape} Tensor not currently implemented")
+        
+        return result
+    
+
+    def _matrix_vector_matrixmultiply(self, other):
         """Temporary method whilst working on code. Here, self in a mxn matrix and other is a nx1 vector"""
         
         assert isinstance(other, Tensor), f"Expected a Tensor, got a {type(other)}"
@@ -74,7 +128,76 @@ class Tensor:
 
         return result
     
-    def vector_vector_add(self, other):
+
+    def _vector_vector_matrixmultiply(self, other):
+        """Temporary method whilst working on code. Here, self in a mxn matrix and other is a nx1 vector"""
+        
+        assert isinstance(other, Tensor), f"Expected a Tensor, got a {type(other)}"
+
+        result_requires_grad = self.requires_grad or other.requires_grad
+        result = Tensor(
+            data = np.dot(self.data, other.data), 
+            requires_grad = result_requires_grad
+        )
+        result._parents += [self, other]
+
+        if self.requires_grad:
+            def vjp():
+                self.grad += result.grad * other.data
+            result._backward_fncs.append(vjp)
+        if other.requires_grad:
+            def vjp():
+                other.grad += result.grad * self.data
+            result._backward_fncs.append(vjp)
+
+        return result
+    
+
+    def _scalar_vector_multiply(self, other):
+        """Temporary method whilst working on code. Here, self is a scalar (e.g. a 0-dim Tensor) and other is a nx1 vector"""
+        
+        assert isinstance(other, Tensor), f"Expected a Tensor, got a {type(other)}"
+        result_requires_grad = self.requires_grad or other.requires_grad
+        result = Tensor(
+            data = self.data * other.data, 
+            requires_grad = result_requires_grad
+        )
+        result._parents += [self, other]   
+    
+        if self.requires_grad:
+            def vjp():
+                self.grad +=  np.dot(result.grad, other.data)
+            result._backward_fncs.append(vjp)
+        if other.requires_grad:
+            def vjp():
+                other.grad += result.grad * self.data 
+            result._backward_fncs.append(vjp)
+
+        return result
+    
+    def _scalar_matrix_multiply(self, other):
+        """Temporary method whilst working on code. Here, self is a scalar (e.g. a 0-dim Tensor) and other is a nx1 vector"""
+        
+        assert isinstance(other, Tensor), f"Expected a Tensor, got a {type(other)}"
+        result_requires_grad = self.requires_grad or other.requires_grad
+        result = Tensor(
+            data = self.data * other.data, 
+            requires_grad = result_requires_grad
+        )
+        result._parents += [self, other]   
+    
+        if self.requires_grad:
+            def vjp():
+                self.grad += np.sum(result.grad * other.data)
+            result._backward_fncs.append(vjp)
+        if other.requires_grad:
+            def vjp():
+                other.grad += result.grad * self.data
+            result._backward_fncs.append(vjp)
+
+        return result
+
+    def _vector_vector_add(self, other):
         """Temporary method whilst working on code. Here, self in a nx1 vector and other is a nx1 vector"""
 
         assert isinstance(other, Tensor), f"Expected a Tensor, got a {type(other)}"
@@ -95,6 +218,7 @@ class Tensor:
             result._backward_fncs.append(vjp)
 
         return result
+    
     
     def relu(self):
         """Temporary method whilst working on code. Here, self in a nx1 vector"""
--- tests/test_autograd_core.py
+++ tests/test_autograd_core.py
@@ -20,20 +20,45 @@ def b1():
 def b2():
     return Tensor(np.array([-10, 20, -3, 0, 1, -1], dtype=np.float32), requires_grad=True) 
 
+@pytest.fixture(scope="function")
+def alpha():
+    return Tensor(3, requires_grad=True)
+
 
 def test_basic_matrix_vector_multiply(W1, x1):
-    y = W1.matrix_vector_multiply(x1) 
+    y = W1 @ x1 
     assert np.allclose(y.data, np.array([5, 11], dtype=np.float32))
     y.backward()
     assert np.allclose(W1.grad, np.array([[1, 2], [1, 2]], dtype=np.float32))
     assert np.allclose(x1.grad, np.array([4, 6], dtype=np.float32))
 
-def test_basic_vector_vector_add_multiply(b1, x1):
-    y = b1.vector_vector_add(x1) 
+def test_basic_vector_vector_add(b1, x1):
+    y = b1 + x1
     assert np.allclose(y.data, np.array([5, 3], dtype=np.float32))
     y.backward()
     assert np.allclose(b1.grad, np.array([1, 1], dtype=np.float32))
     assert np.allclose(x1.grad, np.array([1, 1], dtype=np.float32))
+
+def test_basic_vector_vector_sub(b1, x1):
+    y = b1 - x1 
+    assert np.allclose(y.data, np.array([3, -1], dtype=np.float32))
+    y.backward()
+    assert np.allclose(b1.grad, np.array([1, 1], dtype=np.float32))
+    assert np.allclose(x1.grad, np.array([-1, -1], dtype=np.float32))
+
+def test_basic_scalar_matrix_multiplication(W1, alpha):
+    y1 = alpha * W1
+    assert np.allclose(y1.data, np.array([[3, 6], [9, 12]], dtype=np.float32))
+    y1.backward()
+    assert np.allclose(alpha.grad, 10)
+    assert np.allclose(W1.grad, np.array([[alpha.data, alpha.data], [alpha.data, alpha.data]], dtype=np.float32))
+
+def test_basic_matrix_scalar_multiplication(W1, alpha):
+    y1 = W1 * alpha
+    assert np.allclose(y1.data, np.array([[3, 6], [9, 12]], dtype=np.float32))
+    y1.backward()
+    assert np.allclose(alpha.grad, 10)
+    assert np.allclose(W1.grad, np.array([[alpha.data, alpha.data], [alpha.data, alpha.data]], dtype=np.float32))
 
 def test_basic_relu(b2):
     y = b2.relu()
